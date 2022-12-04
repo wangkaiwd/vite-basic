00b39bd6fefd97362fc60978a5f73e5ab4dbcf12
wangkaiwd <1162486176@qq.com>
2022-12-04 14:33:49 +0800

    feat: revert split chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,12 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import pkg from './package.json';
-import {requireResolve} from './config/utils';
-
-const objectKeys = <T extends object>(object: T): (keyof T)[] => {
-	return Object.keys(object) as (keyof T)[];
-};
-
-type Dependencies = typeof pkg.dependencies
-type Memo = {
-	[k in keyof Dependencies]: string[]
-}
-
-const chunkGroups = objectKeys(pkg.dependencies).reduce((memo, dep) => {
-	memo[dep] = [requireResolve(dep)];
-	return memo;
-}, {} as Memo);
 
 // https://vitejs.dev/config/
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react()
+	],
 	css: {
 		preprocessorOptions: {
 			less: {
