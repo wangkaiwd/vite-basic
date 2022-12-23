6dd379dc1e222ac03f0dd055c45730abf370a4ed
wangkaiwd <1162486176@qq.com>
2022-12-23 17:58:30 +0800

    chore: add some annotation and document

--- packages/rollup-demo/plugins/simple-plugin.ts
+++ packages/rollup-demo/plugins/simple-plugin.ts
@@ -1,19 +1,21 @@
-import {Plugin} from 'rollup';
+import { Plugin } from 'rollup';
 
 const myExample = (): Plugin => ({
-	name: 'my-example', // this name will show up in warnings and errors
-	resolveId(source) {
-		if (source === 'virtual-module') {
-			return source; // this signals that rollup should not ask other plugins or check the file system to find this id
-		}
-		return null; // other ids should be handled as usually
-	},
-	load(id) {
-		if (id === 'virtual-module') {
-			return 'export default "This is virtual!"'; // the source code for "virtual-module"
-		}
-		return null; // other ids should be handled as usually
-	}
+  name: 'my-example', // this name will show up in warnings and errors
+  // define a customer resolver
+  resolveId (source) {
+    if (source === 'virtual-module') {
+      return source; // this signals that rollup should not ask other plugins or check the file system to find this id
+    }
+    return null; // other ids should be handled as usually
+  },
+  // define a custom loader
+  load (id) {
+    if (id === 'virtual-module') {
+      return 'export default "This is virtual!"'; // the source code for "virtual-module"
+    }
+    return null; // other ids should be handled as usually
+  }
 });
 
 export default myExample;
--- packages/rollup-demo/readme.md
+++ packages/rollup-demo/readme.md
@@ -1,2 +1,9 @@
 ## Plugin
 
+### Core Plugin
+
+Try implement core plugin by myself
+
+* [json](https://github.com/rollup/plugins/tree/master/packages/json)
+* [commonjs](https://github.com/rollup/plugins/tree/master/packages/commonjs)
+
